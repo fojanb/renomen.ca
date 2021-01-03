9953b4685aae6783b224d46153584f47b04cef42
Fojan Babaali <fozhanmail@gmail.com>
2021-01-02 23:43:48 -0800

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,173 @@
+/*
+	Stellar by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+function submitQuote(postData){
+	$.ajax({
+		url: "request_quote.php", 
+		data: postData,
+		type: "POST",
+		success: function(result){
+        	$(".modal-body").append(result);
+        	$('#myModal').modal('toggle');
+        	$('#quoteForm')[0].reset();
+    	},
+    	error: function(){
+    		console.log("There was an error");
+    	}
+	});
+}
+
+$('#quoteForm').on("submit", function (e) {
+	e.preventDefault();
+	$(".modal-body").empty();
+	submitQuote($(this).serialize());
+	return false;
+});
+
+(function($) {
+
+	skel.breakpoints({
+		xlarge: '(max-width: 1680px)',
+		large: '(max-width: 1280px)',
+		medium: '(max-width: 980px)',
+		small: '(max-width: 736px)',
+		xsmall: '(max-width: 480px)',
+		xxsmall: '(max-width: 360px)'
+	});
+
+	$(function() {
+
+		var	$window = $(window),
+			$body = $('body'),
+			$main = $('#main'),
+			isDesktop = true;
+
+			$(".filestyle").filestyle('buttonText', 'Choose Photo');
+
+		// Disable animations/transitions until the page has loaded.
+			$body.addClass('is-loading');
+
+			$window.on('load', function() {
+				window.setTimeout(function() {
+					$body.removeClass('is-loading');
+				}, 100);
+			});
+
+		// Fix: Placeholder polyfill.
+			$('form').placeholder();
+
+		// Prioritize "important" elements on medium.
+			skel.on('+medium -medium', function() {
+				$.prioritize(
+					'.important\\28 medium\\29',
+					skel.breakpoint('medium').active
+				);
+			});
+
+		// Nav.
+			isDesktop = (window.innerWidth> 767) ? true : false;
+			var $nav = (isDesktop) ? $('#nav') : $('#navMobile');
+
+			if(!isDesktop){
+				$("#callIcon1").addClass("fa-inverse");
+				$("#callIcon2").addClass("fa-inverse");
+			}else{
+				$("#callIcon1").removeClass("fa-inverse");
+				$("#callIcon2").removeClass("fa-inverse");
+			}
+
+			if ($nav.length > 0) {
+
+				// Shrink effect.
+					$main
+						.scrollex({
+							mode: 'top',
+							enter: function() {
+								$nav.addClass('alt');
+							},
+							leave: function() {
+								$nav.removeClass('alt');
+							},
+						});
+
+				// Links.
+					var $nav_a = $nav.find('a');
+
+					$nav_a
+						.scrolly({
+							speed: 1000,
+							offset: function() { return $nav.height(); }
+						})
+						.on('click', function() {
+
+							var $this = $(this);
+							// External link? Bail.
+								if ($this.attr('href').charAt(0) != '#')
+									return;
+
+							// Deactivate all links.
+								$nav_a
+									.removeClass('active')
+									.removeClass('active-locked');
+
+							// Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
+								$this
+									.addClass('active')
+									.addClass('active-locked');
+								if(!isDesktop){
+									$("#navList").collapse('hide');
+								}
+						})
+						.each(function() {
+
+							var	$this = $(this),
+								id = $this.attr('href'),
+								$section = $(id);
+
+							// No section for this link? Bail.
+								if ($section.length < 1)
+									return;
+
+							// Scrollex.
+								$section.scrollex({
+									mode: 'middle',
+									initialize: function() {
+
+										// Deactivate section.
+											if (skel.canUse('transition'))
+												$section.addClass('inactive');
+
+									},
+									enter: function() {
+
+										// Activate section.
+											$section.removeClass('inactive');
+
+										// No locked links? Deactivate all links and activate this section's one.
+											if ($nav_a.filter('.active-locked').length == 0) {
+
+												$nav_a.removeClass('active');
+												$this.addClass('active');
+
+											}
+
+										// Otherwise, if this section's link is the one that's locked, unlock it.
+											else if ($this.hasClass('active-locked'))
+												$this.removeClass('active-locked');
+
+									}
+								});
+
+						});
+
+			}
+
+		// Scrolly.
+			$('.scrolly').scrolly({
+				speed: 1000
+			});
+	});
+
+})(jQuery);
